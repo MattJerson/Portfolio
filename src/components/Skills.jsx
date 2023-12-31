52eaa018c110f3defc14b1bc43a988e9ca501b1e
Matt Figueroa <79350397+MattJerson@users.noreply.github.com>
2023-12-31 15:36:41 +0800

    Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,17 +10,13 @@ const getIconComponent = (icon) => {
     case "FaCss3Alt":
       return <FaCss3Alt color="#1572B6" size={80}/>;
     case "DiJavascript":
-      return <DiJavascript color="#F7DF1E" size={80}/>;
-    case "FaPhp":
-      return <FaPhp color="#8892BF" size={80}/>;
+      return <DiJavascript color="#F7DF1E" size={80}/>
     case "FaReact":
       return <FaReact color="#61DAFB" size={80}/>;
-    case "FaNodeJs":
-      return <FaNodeJs color="#68A063" size={80}/>;
     case "FaAngular":
       return <FaAngular color="#DD1B16" size={80}/>;
-    case "FaBootstrap":
-      return <FaBootstrap color="#7952B3" size={80}/>;
+    case "FaPython":
+      return <FaPython color="#FFD43B" size={80}/>;
     default:
       return null;
   }
