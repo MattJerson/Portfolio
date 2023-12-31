f3725400f45f28160a32b81c009cdc7e1c621b09
Matt Figueroa <79350397+MattJerson@users.noreply.github.com>
2023-12-31 15:42:16 +0800

    Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -16,7 +16,11 @@ const getIconComponent = (icon) => {
     case "FaAngular":
       return <FaAngular color="#DD1B16" size={80}/>;
     case "FaPython":
-      return <FaPython color="#FFD43B" size={80}/>;
+      return <FaPython style={{
+        background: "linear-gradient(to right, #306998, #FFD43B)",
+        WebkitBackgroundClip: "text",
+        color: "transparent",
+      }} size={80}/>;
     default:
       return null;
   }
