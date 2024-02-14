63447e84f372608be6287bdeeca270cffd4736e2
Matt Figueroa <79350397+MattJerson@users.noreply.github.com>
2024-02-14 13:38:39 +0800

    Update About.jsx

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
               <Info />
             </ul>
             <a href={CV} download='' className="button">
-              Download CV
+              Download Resume
               <span className="button__icon">
                 <FaDownload />
               </span>
